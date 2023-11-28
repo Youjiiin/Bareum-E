2df99edef6c6e86e5d454f7d7b1d5108bfe4a07c
Youjiiin <yujinjang24@gmail.com>
2023-11-29 00:52:20 +0900

    feat: 메인페이지

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Loading from './Pages/Loading';
-import Main from './Pages/Loading';
+import Main from './Pages/Main';
 
 function App() {
   return (
     <BrowserRouter>
-      <div className='.container'>
+      <div className='container'>
         <Routes>
           <Route exact path="/" element={<Loading />}/>
           <Route path='/main' element={<Main />}/>
--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -1,5 +1,18 @@
 import style from "./Main.module.css";
+import mainImg from "../assets/mainImg.png";
 
-const Main = () => {};
+const Main = () => {
+    return <div className={style.wrapper}>
+        <div className={style.title}>
+            <img src={mainImg} alt="main"/>
+            <p>무엇을 해볼까요?</p>
+        </div>
+        
+        <div className={style.btn_container}>
+            <button className={style.btn_study}>공부</button>
+            <button className={style.btn_test}>시험</button>
+        </div>
+    </div>
+};
 
 export default Main;
