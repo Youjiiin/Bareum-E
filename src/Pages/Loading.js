8589e176baae3e7a077f3e959b66050d67717fc3
Youjiiin <yujinjang24@gmail.com>
2023-12-06 19:27:47 +0900

    feat: 로딩페이지 setTimeout 설정

--- src/Pages/Loading.js
+++ src/Pages/Loading.js
@@ -1,12 +1,17 @@
 import { useNavigate } from "react-router-dom";
 import loading from "../assets/loading.png";
 import style from "./Loading.module.css";
+import { useEffect } from "react";
 
 const Main = () => {
     const navigate = useNavigate();
     const mainHandler = () => {
         navigate("/main");
     };
+
+    useEffect(() => {
+        setTimeout(() => {mainHandler()}, 2000);
+    });
 
     return <div className={style.wrapper}>
         <img src={loading} alt="loading" onClick={mainHandler}/>
